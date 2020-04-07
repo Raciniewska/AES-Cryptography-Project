c1b20ec974e951acd4cd0f30063d806e3e65d79a
Raciniewska <raciniewska.barbara@gmail.com>
2020-04-07 13:58:01 +0200

    kod serwera, klucz prywatny i certyfikat

--- server/server/server.cpp
+++ server/server/server.cpp
@@ -1,30 +1,68 @@
+#include <stdio.h>
 #include <iostream>
-#include <iomanip>
-#include <sstream>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <malloc.h>
+#include <string.h>
 #include <string>
 
 using namespace std;
 
-#include <openssl/sha.h>
-string sha256(const string str)
-{
-	unsigned char hash[SHA256_DIGEST_LENGTH];
-	SHA256_CTX sha256;
-	SHA256_Init(&sha256);
-	SHA256_Update(&sha256, str.c_str(), str.size());
-	SHA256_Final(hash, &sha256);
-	stringstream ss;
-	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-	{
-		ss << hex << setw(2) << setfill('0') << (int)hash[i];
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#define FAIL    -1  /*for error output == -1 */
+#define BUFFER 1024  /*buffer for reading messages*/
+#define PORT "1209"
+
+/*creating and setting up a ssl context structure*/
+SSL_CTX* InitServerCTX(void) {	
+
+	const SSL_METHOD* method;
+	SSL_CTX* ctx;
+	
+	SSL_library_init();
+	OpenSSL_add_all_algorithms();   //load & register all cryptos, etc.
+	SSL_load_error_strings();		//lading error messeges
+	
+	method = TLSv1_1_server_method(); //create new server-method instance
+	ctx = SSL_CTX_new(method); //create new context from method
+	
+	if (ctx == NULL) {
+		ERR_print_errors_fp(stderr);
+		abort();
 	}
-	return ss.str();
+	
+	return ctx;
+}
+
+/*to load z certificate into an SSL_CTX structure*/
+void LoadCertificates(SSL_CTX*ctx, char* cert_file, char* key_file) {
+
+	/*to set the local certirficate from cert_file, certificate contains PUBLIC KEY*/
+	if (SSL_CTX_use_certificate_file(ctx, cert_file, SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		abort();
+	}
+	
+	/*to set the PRIVATE KEY fromkey_file*/
+	if (SSL_CTX_use_PrivateKey_file(ctx, key_file, SSL_FILETYPE_PEM)) {
+		ERR_print_errors_fp(stderr);
+		abort();
+	}
+
+	/*to verify private key*/
+	if (!SSL_CTX_check_private_key(ctx)) {
+		cout << "Private key does not match th epublic certificate" << endl;
+		abort();
+	}
 }
 
 int main() {
-	cout << sha256("1234567890_1") << endl;
-	cout << sha256("1234567890_2") << endl;
-	cout << sha256("1234567890_3") << endl;
-	cout << sha256("1234567890_4") << endl;
+	SSL_CTX* ctx;
+	int sever;
+	const char* port_number;
+	port_number = PORT;
+
+	ctx = InitServerCTX();
 	return 0;
 }
